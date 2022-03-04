595c82ca3cd43c74ccd7ab1b41e907af035fa38f
林飞 <fei.l@baisonmail.com>
2022-03-04 18:25:02 +0800

    增加日志输出

--- Services/Stock/DTMStockDemo/Program.cs
+++ Services/Stock/DTMStockDemo/Program.cs
@@ -1,40 +1,69 @@
 using Dtmcli;
 using DTMStockAppService;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Async(c => c.File("Logs/logs.txt"))
+#if DEBUG
+            .WriteTo.Async(c => c.Console())
+#endif
+            .CreateLogger();
 
-// Add services to the container.
-builder.Services.AddDtmcli(x =>
+try
 {
-    x.DtmUrl = "http://localhost:36789";
+    Log.Information("Starting DTMStockDemo.");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddDtmcli(x =>
+    {
+        x.DtmUrl = "http://192.168.148.16:36789";
     // request timeout for DTM server, unit is milliseconds
     x.DtmTimeout = 10000;
 
     // request timeout for trans branch, unit is milliseconds
     x.BranchTimeout = 10000;
-    x.DBType = "sqlserver";
-    x.BarrierTableName = "[dbo].[barrier]";
-});
+        x.DBType = "sqlserver";
+        x.BarrierTableName = "[dbo].[barrier]";
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpApi<IOrderApi>(o => o.HttpHost = new Uri("http://localhost:32004"));
-builder.Services.AddTransient<IStockAppService, StockAppService>();
+    builder.Services.AddHttpApi<IOrderApi>(o => o.HttpHost = new Uri("http://localhost:32004"));
+    builder.Services.AddTransient<IStockAppService, StockAppService>();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly!");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
